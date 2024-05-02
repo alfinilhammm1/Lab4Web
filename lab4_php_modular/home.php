cd6356d8a0d4c9abb42487fc5b36ec6c5c9ea3ad
alfinilhammm1 <149260680+alfinilhammm1@users.noreply.github.com>
2024-05-02 20:55:20 +0700

    Add files via upload

---
+++ lab4_php_modular/home.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman Home Dari Test Website Alfin Ilham Fauzian</h2>
+<p>Ini adalah bagian content dari halaman Website Alfin.</p>
+</div>
+<?php require('footer.php'); ?>
---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,6 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman Kontak Dari Test Website Alfin Ilham Fauzian</h2>
+<p>Ini adalah bagian content dari halaman Kontak Website Alfin.</p>
+</div>
+<?php require('footer.php'); ?>
